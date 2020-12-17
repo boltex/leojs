77e8eb0549879facae9454ca0358e1068f6a8798
Edward K. Ream <edreamleo@gmail.com>
2020-12-17 09:41:55 -0600

    Add transliterations for leoGlobals, sys.inspect

---
+++ src/transliterations/sys/inspect.js
@@ -0,0 +1,8 @@
+/* 000001 */ // Transcrypt'ed from Python, 2020-12-17 09:36:45
+/* 000003 */ import {} from './org.transcrypt.__runtime__.js';
+/* 000001 */ var __name__ = 'inspect';
+/* 000001 */ export var isclass = function (object) {
+/* 000003 */ 	return hasattr (object, '__metaclass__') && !(hasattr (object, '__class__'));
+/* 000003 */ };
+/* 000003 */ 
+//# sourceMappingURL=inspect.map
