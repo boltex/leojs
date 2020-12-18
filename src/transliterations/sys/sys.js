7f1f597bf421a16506388856b4540670b1873c20
Edward K. Ream <edreamleo@gmail.com>
2020-12-18 14:31:11 -0600

    Generate code for node.js

--- src/transliterations/sys/sys.js
+++
@@ -1,5 +0,0 @@
-/* 000001 */ // Transcrypt'ed from Python, 2020-12-17 08:42:32
-/* 000001 */ import {} from './org.transcrypt.__runtime__.js';
-/* 000001 */ var __name__ = 'sys';
-/* 000001 */ 
-//# sourceMappingURL=sys.map
