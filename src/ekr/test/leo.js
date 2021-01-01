3475c529b0932acd5459b2957fc7b44453010304
Edward K. Ream <edreamleo@gmail.com>
2021-01-01 11:47:55 -0600

    Add package.json in src\ekr\test

--- src/ekr/test/leo.js
+++ src/ekr/test/leo.js
@@ -1,6 +1,8 @@
 // leojs.src.ekr.test.leo.js
 
-import { test2 } from "./app";
+// >node --experimental-import-meta-resolve leo.js
+
+import { test2 } from "./app.js";
 
 function test(info) {
     console.log("leo.js:test")
