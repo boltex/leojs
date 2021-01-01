0a1bb34f5563cf280975b33f8c058c9db7b74ada
Edward K. Ream <edreamleo@gmail.com>
2021-01-01 16:57:25 -0600

    Code in ekr/test.node works

---
+++ src/ekr/test.node/app.js
@@ -0,0 +1,7 @@
+// leojs.src.ekr.test.node.app.js
+
+exports.test2 = function() {
+    console.log("app.js:test2");
+}
+
+// export {test2};
---
+++ src/ekr/test.node/leo.js
@@ -0,0 +1,13 @@
+// leojs.src.ekr.test.node.leo.js
+
+app = require("./app")
+
+function test() {
+    console.log("leo.js:test");
+    app.test2();
+}
+
+test();
+
+// export {test};
+
