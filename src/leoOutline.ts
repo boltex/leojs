3812e4e23d4e7375f78df39d4a51a852dc745b26
Félix <felixgraphx@gmail.com>
2022-05-19 23:41:49 -0400

    Redid and optimized the outline provider logic!

--- src/leoOutline.ts
+++ src/leoOutline.ts
@@ -35,32 +35,56 @@ export class LeoOutlineProvider implements vscode.TreeDataProvider<Position> {
         let w_stringId = this.treeId.toString() +
             p_position.v.gnx + p_position.childIndex().toString() +
             p_position.stack.map(p_stackEntry => p_stackEntry[0].gnx + p_stackEntry[1].toString()).join("");
-        // p_collapsed.toString(); // Added Uniqueness:  VSCode's collapsible state linked to id
+        // NOT NEEDED -> p_collapsed.toString(); // Added Uniqueness: VSCode's collapsible state in id
         return w_stringId;
     }
 
+    /**
+     * * Force uniqueness of ids generated for nodes in the  next tree refresh
+     */
     public incTreeId(): void {
         this.treeId++;
     }
 
     public getTreeItem(element: Position): Thenable<LeoOutlineNode> | LeoOutlineNode {
+
         let w_collapse: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.None;
         if (element.hasChildren()) {
             w_collapse = element.isExpanded() ? vscode.TreeItemCollapsibleState.Expanded : vscode.TreeItemCollapsibleState.Collapsed;
         }
+        let w_contextValue = Constants.CONTEXT_FLAGS.NODE_UNMARKED;
+        if (element.isMarked()) {
+            w_contextValue = Constants.CONTEXT_FLAGS.NODE_MARKED;
+        }
+        // then append 'leoNodeAtFile' to existing if needed
+        if (element.isAnyAtFileNode()) {
+            w_contextValue += Constants.CONTEXT_FLAGS.NODE_ATFILE;
+        }
+        // then append 'leoNodeCloned' to existing if needed
+        if (element.isCloned()) {
+            w_contextValue += Constants.CONTEXT_FLAGS.NODE_CLONED;
+        }
+        // and finally, check for 'root' too
+        if (element._isRoot) {
+            w_contextValue += Constants.CONTEXT_FLAGS.NODE_ROOT;
+        } else {
+            w_contextValue += Constants.CONTEXT_FLAGS.NODE_NOT_ROOT;
+        }
+        const w_icon: number =
+            (+element.isDirty() << 3) |
+            (+element.isCloned() << 2) |
+            (+element.isMarked() << 1) |
+            +element.v.hasBody();
+        const w_u = Object.keys(element.v.u).length ? element.v.u : false;
+
         const w_leoNode = new LeoOutlineNode(
             element.h,
-            w_collapse, // element.hasChildren() ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None,
-            element, // ap
-            element.isCloned(), // cloned
-            element.isDirty(), // dirty
-            element.isMarked(), // marked
-            element.isAnyAtFileNode(), // atFile
-            element.v.hasBody(),
-            Object.keys(element.v.u).length ? element.v.u : false, // 'u' - user defined data
-            this._icons,
+            w_collapse,
+            element, // Position
+            w_u ? "\u{1F4CE} (" + Object.keys(w_u).length + ")" : "",
+            this._icons[w_icon],
             this.buildId(element, w_collapse),
-            element._isRoot
+            w_contextValue
         );
         // Check if its the selected node and call signal it to the UI
         if (element.__eq__(g.app.windowList[this._leoUI.frameIndex].c.p)) {
@@ -109,130 +133,35 @@ export class LeoOutlineProvider implements vscode.TreeDataProvider<Position> {
         return undefined;
     }
 
+    public resolveTreeItem(item: LeoOutlineNode, element: Position, token: vscode.CancellationToken): vscode.ProviderResult<LeoOutlineNode> {
+        if (Object.keys(element.v.u).length) {
+            item.tooltip = item.label + "\n" +
+                JSON.stringify(element.v.u, undefined, 2);
+        } else {
+            item.tooltip = item.label; // * Whole headline as tooltip
+        }
+        return item;
+    }
+
 }
 
 export class LeoOutlineNode extends vscode.TreeItem {
 
-    public contextValue: string; // * Context string is checked in package.json with 'when' clauses
-
     constructor(
         public label: string, // Node headline
-        public collapsibleState: vscode.TreeItemCollapsibleState, // Computed in receiver/creator
+        public collapsibleState: vscode.TreeItemCollapsibleState,
         public position: Position, // Pointer/reference for leo's node position
-        public cloned: boolean,
-        public dirty: boolean,
-        public marked: boolean,
-        public atFile: boolean,
-        public hasBody: boolean,
-        public u: any,
-        private _icons: Icon[], // pointer to global array of node icons
-        private _id: string,
-        public isRoot: boolean // For contextual menu on each node (not the global selected node flag!)
+        public description: string,
+        public iconPath: Icon,
+        public id: string,
+        public contextValue: string // For contextual menu on each node (not the global 'selected node' flag!)
     ) {
         super(label, collapsibleState);
-        this.contextValue = this._getNodeContextValue();
         this.command = {
             command: Constants.COMMANDS.SELECT_NODE,
             title: '',
             arguments: [this]
         };
-    }
-
-    // * TO HELP DEBUG
-    // get description(): string {
-    //     // * some smaller grayed-out text accompanying the main label
-    //     const w_ap: ArchivedPosition = JSON.parse(this.ap);
-    //     return "child:" + w_ap.childIndex + " lvl:" + w_ap.level + " gnx:" + w_ap.gnx;
-    // }
-
-    // get description(): string {
-    //     // * some smaller grayed-out text accompanying the main label
-    //     return "id:" + this.id;
-    // }
-
-    /**
-     * * Sets this node properties (dirty, marked, etc.) by copying from a given node.
-     * * This is needed by the outline provider when refreshing a single node.
-     * @param p_node Node to copy properties from.
-     * @returns Node itself with the new properties applied
-     */
-    public copyProperties(p_node: LeoOutlineNode): LeoOutlineNode {
-        this.label = p_node.label;
-        this.collapsibleState = p_node.collapsibleState;
-        this.position = p_node.position;
-        this.cloned = p_node.cloned;
-        this.dirty = p_node.dirty;
-        this.marked = p_node.marked;
-        this.atFile = p_node.atFile;
-        this.hasBody = p_node.hasBody;
-        this.isRoot = p_node.isRoot;
-        this.contextValue = this._getNodeContextValue();
-        return this;
-    }
-
-    private _getNodeContextValue(): string {
-        // Start it with 'leoNodeMarked' or 'leoNodeUnmarked'
-        let w_contextValue = Constants.CONTEXT_FLAGS.NODE_UNMARKED;
-        if (this.marked) {
-            w_contextValue = Constants.CONTEXT_FLAGS.NODE_MARKED;
-        }
-        // then append 'leoNodeAtFile' to existing if needed
-        if (this.atFile) {
-            w_contextValue += Constants.CONTEXT_FLAGS.NODE_ATFILE;
-        }
-        // then append 'leoNodeCloned' to existing if needed
-        if (this.cloned) {
-            w_contextValue += Constants.CONTEXT_FLAGS.NODE_CLONED;
-        }
-        // and finally, check for 'root' too
-        if (this.isRoot) {
-            w_contextValue += Constants.CONTEXT_FLAGS.NODE_ROOT;
-        } else {
-            w_contextValue += Constants.CONTEXT_FLAGS.NODE_NOT_ROOT;
-        }
-        return w_contextValue;
-    }
-
-    // @ts-ignore
-    public get iconPath(): Icon {
-        // From Leo's leoNodes.py computeIcon function
-        // 1=has Body, 2=marked, 4=cloned, 8=dirty
-        let w_icon: number =
-            (+this.dirty << 3) |
-            (+this.cloned << 2) |
-            (+this.marked << 1) |
-            +this.hasBody;
-        return this._icons[w_icon];
-    }
-
-    // Optional id for the tree item that has to be unique across tree.
-    // The id is used to preserve the selection and expansion state of the tree item.
-    // If not provided, an id is generated using the tree item's label.
-    // Note that when labels change, ids will change and that selection and expansion state cannot be kept stable anymore.
-    // @ts-ignore
-    public get id(): string { return this._id; }
-
-    // @ts-ignore
-    public get description(): string {
-        // * some smaller grayed-out text accompanying the main label
-        if (this.u) {
-            return "\u{1F4CE} (" + Object.keys(this.u).length + ")";
-        } else {
-            // return "id:" + this.id; // ! debug test
-            // return "gnx:" + this.gnx; // ! debug test
-            return ""; // Falsy will not be shown
-        }
-    }
-
-    // @ts-ignore
-    public get tooltip(): string {
-        if (this.u) {
-            //  "\ntotal keys is :" + Object.keys(this.u).length
-            return this.label + "\n" +
-                JSON.stringify(this.u, undefined, 2);
-        } else {
-            return this.label; // * Whole headline as tooltip
-        }
     }
 
 }
