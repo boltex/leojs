35c903dcef8feafbe9d687e4f3bc10ab9ee747c5
Félix <felixgraphx@gmail.com>
2023-07-23 20:04:25 -0400

    Added unit tests for leoCompare.ts

--- src/test/leoCompare.test.ts
+++ src/test/leoCompare.test.ts
@@ -3,11 +3,16 @@
 /**
  * Tests of leoCompare.ts
  */
+import * as vscode from "vscode";
 import * as assert from 'assert';
 import { afterEach, before, beforeEach } from 'mocha';
 
 import * as g from '../core/leoGlobals';
 import { LeoUnitTest } from './leoTest2';
+import { Position } from '../core/leoNodes';
+import { CompareLeoOutlines } from '../core/leoCompare';
+import * as path from 'path';
+import * as os from 'os';
 
 //@+others
 //@+node:felix.20230723005339.2: ** suite TestCompare
@@ -33,73 +38,74 @@ suite('Test cases for leoCompare.ts', () => {
     //@+others
     //@+node:felix.20230723005339.3: *3* TestCompare.test_diff_marked_nodes
     test('test_diff_marked_nodes', () => {
-        console.log('TODO : test_diff_marked_nodes');
 
-        /*
-        from leo.core.leoCompare import diffMarkedNodes
-        
-        # Setup.
-        c = self.c
-        u = c.undoer
-        root = c.rootPosition()
-        root.deleteAllChildren()
-        while root.hasNext():
-            root.next().doDelete()
-        c.selectPosition(root)
-        
-        # Create two sets of nodes.
-        node1 = root.insertAsLastChild()
-        node2 = root.insertAsLastChild()
-        child1 = node1.insertAsLastChild()
-        child2 = node2.insertAsLastChild()
-        
-        # Mark the nodes.
-        node1.setMarked()
-        node2.setMarked()
-        
-        # Populate the nodes.
-        table = (
-            (node1, 'node 1', '# Node 1.\n'),
-            (node2, 'node 1a', '# Node 1.\n'),  # Headlines differ.
-            (child1, 'child 1', '# Child 1.\n'),
-            (child2, 'child 1', '# Child 1a.\n'),  # Bodies differ.
-        )
-        for p, h, b in table:
-            p.h = h
-            p.b = b
-        self.assertEqual(c.lastTopLevel(), root)
+        // from leo.core.leoCompare import diffMarkedNodes
 
-        # Run the command.
-        diffMarkedNodes(event={'c': c})
-        self.assertEqual(c.lastTopLevel().h, 'diff marked nodes')
-        u.undo()
-        self.assertEqual(c.lastTopLevel(), root)
-        u.redo()
-        self.assertEqual(c.lastTopLevel().h, 'diff marked nodes')
-        */
+        // Setup.
+        const c = self.c;
+        const u = c.undoer;
+        const root = c.rootPosition()!;
+        root.deleteAllChildren();
+        while (root.hasNext()) {
+            root.next().doDelete();
+        }
+        c.selectPosition(root);
+
+        // Create two sets of nodes.
+        const node1 = root.insertAsLastChild();
+        const node2 = root.insertAsLastChild();
+        const child1 = node1.insertAsLastChild();
+        const child2 = node2.insertAsLastChild();
+
+        // Mark the nodes.
+        node1.setMarked();
+        node2.setMarked();
+
+        // Populate the nodes.
+        const table: [Position, string, string][] = [
+            [node1, 'node 1', '# Node 1.\n'],
+            [node2, 'node 1a', '# Node 1.\n'],  // Headlines differ.
+            [child1, 'child 1', '# Child 1.\n'],
+            [child2, 'child 1', '# Child 1a.\n'],  // Bodies differ.
+        ];
+        for (const [p, h, b] of table) {
+            p.h = h;
+            p.b = b;
+        }
+        assert.ok(c.lastTopLevel().__eq__(root));
+
+        // Run the command.
+        c.diffMarkedNodes();
+        assert.strictEqual(c.lastTopLevel().h, 'diff marked nodes')
+
+        u.undo();
+        assert.ok(c.lastTopLevel().__eq__(root));
+
+        u.redo();
+        assert.strictEqual(c.lastTopLevel().h, 'diff marked nodes');
 
     });
     //@+node:felix.20230723005339.4: *3* TestCompare.test_diff_list_of_files
-    test('test_diff_list_of_files', () => {
-        console.log('TODO : test_diff_list_of_files');
+    test('test_diff_list_of_files', async () => {
 
-        /*
-        from leo.core.leoCompare import CompareLeoOutlines
+        // from leo.core.leoCompare import CompareLeoOutlines
 
-        # Setup.
-        c = self.c
-        u = c.undoer
-        x = CompareLeoOutlines(c)
-        root = c.rootPosition()
-        root.deleteAllChildren()
-        while root.hasNext():
-            root.next().doDelete()
-        c.selectPosition(root)
-        self.assertEqual(c.lastTopLevel(), root)
-        
-        # The contents of a small .leo file.
-        contents1 = textwrap.dedent(
-            """
+        // Setup.
+        const c = self.c;
+        const u = c.undoer;
+        const x = new CompareLeoOutlines(c);
+        const root = c.rootPosition()!;
+        root.deleteAllChildren();
+        while (root.hasNext()) {
+            root.next().doDelete();
+        }
+
+        c.selectPosition(root);
+        assert.ok(c.lastTopLevel().__eq__(root));
+
+        // The contents of a small .leo file.
+        const contents1 = g.dedent(
+            `\
             <?xml version="1.0" encoding="utf-8"?>
             <!-- Created by Leo: https://leo-editor.github.io/leo-editor/leo_toc.html -->
             <leo_file xmlns:leo="http://leo-editor.github.io/leo-editor/namespaces/leo-python-editor/1.1" >
@@ -114,38 +120,53 @@ suite('Test cases for leoCompare.ts', () => {
             <t tx="ekr.20230714162224.2"></t>
             </tnodes>
             </leo_file>
-            """).lstrip()  # Leo doesn't tolerate a leading blank line!
-        contents2 = contents1.replace('test_file1.leo', 'test_file2.leo')
-        
-        # Create the absolute paths.
-        directory = tempfile.gettempdir()
-        path1 = os.path.normpath(os.path.join(directory, 'test_file1.leo'))
-        path2 = os.path.normpath(os.path.join(directory, 'test_file2.leo'))
-        paths = [path1, path2]
+            `
+        ).trimStart();  // Leo doesn't tolerate a leading blank line!
+        const contents2 = contents1.replace('test_file1.leo', 'test_file2.leo');
 
-        # Create two temp .leo files.
-        for path, contents in ((path1, contents1), (path2, contents2)):
-            with open(path, 'wb') as f:
-                f.write(g.toEncodedString(contents))
+        // Create the absolute paths.
+        const directory = os.tmpdir(); //tempfile.gettempdir();
+        const path1 = g.os_path_normpath(path.join(directory, 'test_file1.leo'));
+        const path2 = g.os_path_normpath(path.join(directory, 'test_file2.leo'));
+        const paths = [path1, path2];
 
-        # Run the command.
-        expected_last_headline = 'diff-leo-files'
-        x.diff_list_of_files(paths)
-        self.assertEqual(c.lastTopLevel().h, expected_last_headline)
-        
-        # Test undo and redo.
-        u.undo()
-        self.assertEqual(c.lastTopLevel(), root)
-        u.redo()
-        self.assertEqual(c.lastTopLevel().h, expected_last_headline)
-        
-        # Remove temporary files.
-        for path in paths:
-            self.assertTrue(os.path.exists(path), msg=path)
-            os.remove(path)
-            self.assertFalse(os.path.exists(path), msg=path)
-        */
+        // Create two temp .leo files.
+        for (const [w_path, contents] of [[path1, contents1], [path2, contents2]]) {
 
+
+            const w_writeUri = g.makeVscodeUri(w_path);
+            const writeData = g.toEncodedString(contents);
+
+            await vscode.workspace.fs.writeFile(w_writeUri, writeData);
+
+            // with open(w_path, 'wb') as f
+            //     f.write(g.toEncodedString(contents));
+
+        }
+        // Run the command.
+        const expected_last_headline = 'diff-leo-files';
+        await x.diff_list_of_files(paths);
+        assert.strictEqual(c.lastTopLevel().h, expected_last_headline);
+
+        // Test undo and redo.
+        u.undo();
+        assert.ok(c.lastTopLevel().__eq__(root));
+        u.redo();
+        assert.strictEqual(c.lastTopLevel().h, expected_last_headline);
+
+        // Remove temporary files.
+        for (const w_path of paths) {
+            let w_exists = await g.os_path_exists(w_path);
+            assert.ok(w_exists, w_path);
+
+            const w_uri = g.makeVscodeUri(w_path);
+
+            await vscode.workspace.fs.delete(w_uri, { recursive: true });
+            // os.remove(w_path);
+
+            w_exists = await g.os_path_exists(w_path);
+            assert.ok(!w_exists, w_path);
+        }
     });
     //@-others
 
