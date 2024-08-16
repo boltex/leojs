8ebc343f176583e75efda5234d692d784249d516
Félix <felixgraphx@gmail.com>
2024-08-16 00:23:55 -0400

    Fixed LeoJS docs layout and styling

---
+++ leojs-docs/src/theme/DocSidebar/index.tsx
@@ -0,0 +1,29 @@
+/**
+ * This file wraps the original DocSidebar so we don't need to modify the original code.
+ *
+ * Reason for modifying:
+ * - Add a logo to the top of the sidebar
+ */
+
+import React from 'react';
+import DocSidebar from '@theme-original/DocSidebar';
+import type { Props } from '@theme/DocSidebar';
+
+import Logo from '@theme-original/Logo';
+
+export default function DocSidebarWrapper(props: Props): JSX.Element {
+    return (
+      <>
+        <DocSidebar {...props} />
+      </>
+    );
+  }
+
+// export default function DocSidebarWrapper(props: Props): JSX.Element {
+//   return (
+//     <>
+//       <Logo />
+//       <DocSidebar {...props} />
+//     </>
+//   );
+// }
---
+++ leojs-docs/src/theme/Navbar/Content/index.tsx
@@ -0,0 +1,88 @@
+import React, {type ReactNode} from 'react';
+import {useThemeConfig, ErrorCauseBoundary} from '@docusaurus/theme-common';
+import {
+  splitNavbarItems,
+  useNavbarMobileSidebar,
+} from '@docusaurus/theme-common/internal';
+import { useLocation } from 'react-router-dom'; // Import useLocation
+import NavbarItem, {type Props as NavbarItemConfig} from '@theme/NavbarItem';
+import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
+import SearchBar from '@theme/SearchBar';
+import NavbarMobileSidebarToggle from '@theme/Navbar/MobileSidebar/Toggle';
+import NavbarLogo from '@theme/Navbar/Logo';
+import NavbarSearch from '@theme/Navbar/Search';
+
+import styles from './styles.module.css';
+
+function useNavbarItems() {
+  return useThemeConfig().navbar.items as NavbarItemConfig[];
+}
+
+function NavbarItems({items}: {items: NavbarItemConfig[]}): JSX.Element {
+  return (
+    <>
+      {items.map((item, i) => (
+        <ErrorCauseBoundary
+          key={i}
+          onError={(error) =>
+            new Error(
+              `A theme navbar item failed to render.
+Please double-check the following navbar item (themeConfig.navbar.items) of your Docusaurus config:
+${JSON.stringify(item, null, 2)}`,
+              {cause: error},
+            )
+          }>
+          <NavbarItem {...item} />
+        </ErrorCauseBoundary>
+      ))}
+    </>
+  );
+}
+
+function NavbarContentLayout({
+  left,
+  right,
+}: {
+  left: ReactNode;
+  right: ReactNode;
+}) {
+  return (
+    <div className="navbar__inner">
+      <div className="navbar__items">{left}</div>
+      <div className="navbar__items navbar__items--right">{right}</div>
+    </div>
+  );
+}
+
+export default function NavbarContent(): JSX.Element {
+  const mobileSidebar = useNavbarMobileSidebar();
+
+  const items = useNavbarItems();
+  const [leftItems, rightItems] = splitNavbarItems(items);
+
+  const searchBarItem = items.find((item) => item.type === 'search');
+  const location = useLocation(); // Get the current URL pathname
+
+  return (
+    <NavbarContentLayout
+      left={
+        <>
+          {!mobileSidebar.disabled && <NavbarMobileSidebarToggle />}
+          {location.pathname === '/' && <NavbarLogo />} {/* Conditionally render Logo */}
+          <NavbarItems items={leftItems} />
+        </>
+      }
+      right={
+        <>
+          <NavbarItems items={rightItems} />
+          <NavbarColorModeToggle className={styles.colorModeToggle} />
+          {!searchBarItem && (
+            <NavbarSearch>
+              <SearchBar />
+            </NavbarSearch>
+          )}
+        </>
+      }
+    />
+  );
+}
