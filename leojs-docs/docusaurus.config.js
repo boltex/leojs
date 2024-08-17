9dbf2b31c51bf0d0c2640cc81faae31ff31e2375
Félix <felixgraphx@gmail.com>
2024-08-17 15:27:51 -0400

    Made progress on structure of documentation folders.

--- leojs-docs/docusaurus.config.js
+++ leojs-docs/docusaurus.config.js
@@ -14,8 +14,10 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://felixworkshop.com/',
+
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
+  // baseUrl: '/leojs/', // ! TO DEPLOY IN /leojs/
   baseUrl: '/',
 
   // GitHub pages deployment config.
@@ -41,10 +43,6 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: false,
         // blog: {
@@ -53,11 +51,6 @@ const config = {
         //     type: ['rss', 'atom'],
         //     xslt: true,
         //   },
-        //   // Please change this to your repo.
-        //   // Remove this to remove the "edit this page" links.
-        //   // editUrl:
-        //   //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -78,11 +71,35 @@ const config = {
           src: 'img/logo.svg',
         },
         items: [
+          // {
+          //   type: 'docSidebar',
+          //   sidebarId: 'tutorialSidebar',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
           {
             type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
+            sidebarId: 'gettingStartedSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Getting Started',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'usersGuideSidebar',
+            position: 'left',
+            label: 'User\'s Guide',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'advancedTopicsSidebar',
+            position: 'left',
+            label: 'Advanced Topics',
+          },
+          {
+            type: 'docSidebar',
+            sidebarId: 'appendicesSidebar',
+            position: 'left',
+            label: 'Appendices',
           },
           // { to: '/blog', label: 'Blog', position: 'left' },
           {
@@ -92,6 +109,14 @@ const config = {
           },
         ],
       },
+      // announcementBar: {
+      //   id: 'leojs_available',
+      //   content:
+      //     'LeoJS is now available in the VSCode marketplace, and in VSCodium\'s Open VSX Registry!',
+      //   backgroundColor: '#fafbfc',
+      //   textColor: '#091E42',
+      //   isCloseable: false,
+      // },
       // footer: {
       //   style: "dark", // Comment off for customized styles from custom.css
       //   links: [
