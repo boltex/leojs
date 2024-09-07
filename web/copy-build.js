ebaaa740914cb7bf23362f1fc72c1f0c85ccb9f7
Félix <felixgraphx@gmail.com>
2024-09-06 23:31:31 -0400

    Improved documentation build/deploy script.

--- web/copy-build.js
+++ web/copy-build.js
@@ -5,11 +5,20 @@ const path = require('path');
 const sourceDir = path.join(__dirname, 'build');
 const targetDir = path.join(__dirname, '..', 'docs');
 
-// Copy the directory
-fs.copy(sourceDir, targetDir, function (err) {
+// Empty the target directory first
+fs.emptyDir(targetDir, function (err) {
     if (err) {
-        console.error('Error copying files:', err);
+        console.error('Error clearing the target directory:', err);
         return;
     }
-    console.log('Build directory copied successfully!');
+    console.log('Target directory cleared.');
+
+    // Copy the directory
+    fs.copy(sourceDir, targetDir, function (err) {
+        if (err) {
+            console.error('Error copying files:', err);
+            return;
+        }
+        console.log('Build directory copied successfully!');
+    });
 });
