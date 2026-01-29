1c2cb868cb0499256a4580548839dc3d75dfeaae
Félix <felixgraphx@gmail.com>
2026-01-28 23:37:06 -0500

    Fixed expandToLevel.

--- src/core/leoCommands.ts
+++ src/core/leoCommands.ts
@@ -4763,9 +4763,8 @@ export class Commands {
         }
         c.expansionNode = c.p.copy();
         c.expansionLevel = max_level + 1;
-        if (c.expansionLevel !== old_expansion_level) {
-            c.redraw();
-        }
+        c.redraw();
+
         /*
         // It's always useful to announce the level.
         // c.k.setLabelBlue('level: %s' % (max_level+1))
